39c4618d13fd0f285c6ae64b3a98f1922de72dc1
yileicheng <yilei.cheng.cyl@gmail.com>
2024-09-30 18:59:46 +0200

    feat: update cached-speed-compare

--- cached-speed-compare.js
+++ cached-speed-compare.js
@@ -1,50 +1,39 @@
 const { execSync } = require("child_process");
 const Table = require("cli-table3");
 
-const packageManagers = [
-  { name: "npm", installCmd: "npm install lodash" },
-  { name: "pnpm", installCmd: "pnpm add lodash" },
-  { name: "yarn", installCmd: "yarn add lodash" },
-];
-
-const repetitions = 5;
-
-const timeInstall = (installCmd) => {
-  const times = [];
-
-  for (let i = 0; i < repetitions; i++) {
-    console.log(`Running "${installCmd}" - iteration ${i + 1}`);
-
-    const startTime = process.hrtime();
-    execSync(installCmd, { stdio: "ignore" });
-    const endTime = process.hrtime(startTime);
-
-    const timeInSeconds = endTime[0] + endTime[1] / 1e9;
-    times.push(timeInSeconds);
-  }
-
-  const averageTime = times.reduce((sum, time) => sum + time, 0) / repetitions;
-  return averageTime;
+// Define package manager commands
+const commands = {
+  pnpm: "pnpm add react",
+  npm: "npm install react",
+  yarn: "yarn add react",
 };
 
-const results = packageManagers.map((pm) => {
-  console.log(`Cleaning up using: node reset.js`);
-  execSync("node reset.js");
+function measureTime(command) {
+  execSync(command, { stdio: "ignore" });
+  execSync("rm -rf node_modules", { stdio: "ignore" });
 
-  const avgTimeTaken = timeInstall(pm.installCmd);
-  return { manager: pm.name, avgTime: avgTimeTaken };
-});
+  const startTime = process.hrtime();
+  execSync(command, { stdio: "ignore" });
+  const endTime = process.hrtime(startTime);
+
+  const timeInSeconds = endTime[0] + endTime[1] / 1e9;
+  return timeInSeconds;
+}
+
+const results = [];
+
+for (const manager in commands) {
+  const time = measureTime(commands[manager]);
+  results.push({ packageManager: manager, timeInSeconds: time });
+}
 
 const table = new Table({
-  head: ["Package Manager", `Avg Time (${repetitions} runs)`],
-  colWidths: [20, 30],
+  head: ["Package Manager", "Time (seconds)"],
+  colWidths: [20, 15],
 });
 
 results.forEach((result) => {
-  table.push([
-    result.manager,
-    result.avgTime ? result.avgTime.toFixed(3) : "Error",
-  ]);
+  table.push([result.packageManager, result.timeInSeconds.toFixed(2) + "s"]);
 });
 
 console.log(table.toString());
